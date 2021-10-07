329699f549d055195da3d939626fbf7303f95e04
Alan Ibarra <alan.ibarra209@gmail.com>
2021-10-07 00:07:05 -0700

    workshop complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
-
+import GreyBackground from './components/GreyBackground';
 function App() {
   return (
     <>
+      <GreyBackground />
       <CharacterEditor />
       <Footer />
+      
     </>
   );
 }
---
+++ src/components/GreyBackground/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from "./GreyBackground.module.css"
+const GreyBackground = () => {
+    return <div className={`${styles.greyBackground}`}></div>
+}
+
+export default GreyBackground;
